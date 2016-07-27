c1f01a68c7565e07dd2f6f41c62783cba50a9aa1
Lapointe Quentin <q.lapointe@gmail.com>
2016-07-27 15:45:55 +0200

    maj v2.3 + favicon

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# resume
+Curriculum Vitæ
--- js/languages.js
+++ js/languages.js
@@ -2,11 +2,11 @@ var dictionary =
 {
 	"fr":{
 		"title":[
-			"<i class=\"fa fa-graduation-cap\"></i>" + "Formation",
-			"<i class=\"fa fa-suitcase\"></i>" + "Expériences professionnelles",
-			"<i class=\"fa fa-tachometer\"></i>" + "Compétences",
-			"<i class=\"fa fa-flag\"></i>" + "Langues étrangères",
-			"<i class=\"fa fa-magic\"></i>" + "Spécificités personnelles",
+			"<i class=\"fa fa-graduation-cap\"></i>Formation",
+			"<i class=\"fa fa-suitcase\"></i>Expériences professionnelles",
+			"<i class=\"fa fa-tachometer\"></i>Compétences",
+			"<i class=\"fa fa-flag\"></i>Langues",
+			"<i class=\"fa fa-magic\"></i>Spécificités personnelles",
 			""
 		],
 		"name":[
@@ -14,38 +14,56 @@ var dictionary =
 			"DUT Informatique",
 			"Tronc commun",
 			"Baccalauréat Scientifique",
-			"POC sur la base de données graphes " + "<a href=\"http://neo4j.com/\"><i class=\"fa fa-link\"></i> Neo4j</a>",
+
+			"POC sur <a href=\"http://neo4j.com/\"><i class=\"fa fa-link\"></i> Neo4j</a>",
+			"POC sur <a href=\"http://neo4j.com/\"><i class=\"fa fa-link\"></i> Neo4j</a>",
 			"Application iOS",
 			"Application interne de collaboration",
+
 			"Programmation",
+			"Web",
 			"Outils",
 			"Systèmes d'exploitation",
+
 			"Français",
 			"Anglais",
 			"Espagnol",
+
 			"Parachutiste",
-			"",
-			"Ramasseur de balles"
+			"Compétition inter-IUT — 24h de l'informatique",
+			"Babysitting & Cours de Mathématiques",
+			"Ramasseur de balles",
+			"Passions"
 		],
 		"description":[
 			"Mention bien — 15,92",
-			"Détection d'incohérences au sein d'une BD relationnelle par sa transposition en BD orientée graphes",
-			"Développement de " + "<a href=\"https://itunes.apple.com/fr/app/togafmore/id581774033\"><i class=\"fa fa-link\"></i> TogafMore</a>",
+
+			"E-marketing à l'aide d'une base de donnée graphes<ul><li>Liaisons des données CRM et de navigation Web de différents clients</li><li>Récupération d'une liste de contacts par calcul du chemin le moins cher en fonction d'une matrice de prix</li></ul>",
+			"Détection d'incohérences au sein d'une BD relationnelle en la transposant en BD orientée graphes<ul><li>Modélisation de la BD & ingestion des données</li><li>Détection d'incohérences & propositions de rectification</li><li>Statistiques sur les audiences Internet multi-support</li></ul>",
+			"Développement de <a href=\"https://itunes.apple.com/fr/app/togafmore/id581774033\"><i class=\"fa fa-link\"></i> TogafMore</a>",
+			"",
+
 			"",
 			"",
 			"",
 			"",
+
 			"Langue maternelle",
 			"Courant — TOEIC 890 points",
 			"Niveau B1",
+
 			"10 sauts autonomes",
-			"En compétition",
-			""
+			"",
+			"",
+			"",
+			"Musique — Théâtre — Piano — Judo — Tennis — Football"
 		],
 		"place":[
 			"",
 			"IUT de Vélizy-Villacoublay",
 			"Université de Technologie de Compiègne",
+			"",
+
 			"",
 			"",
 			"",
@@ -57,18 +75,20 @@ var dictionary =
 			"",
 			"",
 			"",
-			"Avril - Juillet 2015",
+
+			"Juin – Juillet 2016",
+			"Avril – Juillet 2015",
 			"Juillet 2014",
 			"Juillet 2012"
 		]
 	},
 	"en":{
 		"title":[
-			"<i class=\"fa fa-graduation-cap\"></i>" + "Education",
-			"<i class=\"fa fa-suitcase\"></i>" + "Work experience",
-			"<i class=\"fa fa-tachometer\"></i>" + "Skills",
-			"<i class=\"fa fa-flag\"></i>" + "Foreign languages",
-			"<i class=\"fa fa-magic\"></i>" + "Interests & activities",
+			"<i class=\"fa fa-graduation-cap\"></i>Education",
+			"<i class=\"fa fa-suitcase\"></i>Work experience",
+			"<i class=\"fa fa-tachometer\"></i>Skills",
+			"<i class=\"fa fa-flag\"></i>Languages",
+			"<i class=\"fa fa-magic\"></i>Interests & activities",
 			""
 		],
 		"name":[
@@ -76,38 +96,55 @@ var dictionary =
 			"Two-year university degree in computer science",
 			"Core curriculum",
 			"Science baccalaureate",
-			"POC on the graph database " + "<a href=\"http://neo4j.com/\"><i class=\"fa fa-link\"></i> Neo4j</a>",
+
+			"POC on <a href=\"http://neo4j.com/\"><i class=\"fa fa-link\"></i> Neo4j</a>",
+			"POC on <a href=\"http://neo4j.com/\"><i class=\"fa fa-link\"></i> Neo4j</a>",
 			"iOS application",
 			"Internal application of collaboration",
+
 			"Programming",
+			"Web",
 			"Tools",
 			"Operating systems",
+
 			"Français",
 			"English",
 			"Spanish",
+
 			"Parachutist",
-			"",
-			"Ball boy"
+			"Interschool competition — 24h of computing",
+			"Babysitting & Mathematics lessons",
+			"Ball boy",
+			""
 		],
 		"description":[
 			"With honours",
-			"Detection of inconsistencies in a relational database by transposing it in a graph oriented database",
-			"Development of " + "<a href=\"https://itunes.apple.com/fr/app/togafmore/id581774033\"><i class=\"fa fa-link\"></i> TogafMore</a>",
+
+			"E-marketing with graph-oriented database<ul><li>Linking CRM and Web navigation data from several clients</li><li>Collecting a list of contacts by computing the least expensive path according to a price matrix</li></ul>",
+			"Inconsistencies detection in a relational database by transposing it in a graph oriented database<ul><li>Database modeling & data ingestion</li><li>Inconsistencies detection & rectification proposals</li><li>Statistics over multisupport Internet audiences</li></ul>",
+			"Development of <a href=\"https://itunes.apple.com/fr/app/togafmore/id581774033\"><i class=\"fa fa-link\"></i> TogafMore</a>",
+			"",
+
 			"",
 			"",
 			"",
 			"",
+
 			"Mother tongue",
 			"Fluent — Good communication skills",
 			"Basic notions",
+
 			"10 autonomous jumps",
-			"Competition level",
-			""
+			"",
+			"",
+			"",
+			"Music — Theatre — Piano — Judo — Tennis — Soccer"
 		],
 		"place":[
 			"",
 			"UIT of Vélizy-Villacoublay",
 			"University of Technology of Compiègne",
+			"",
 			"",
 			"",
 			"",
@@ -119,6 +156,7 @@ var dictionary =
 			"",
 			"",
 			"",
+			"June - July 2016",
 			"April - July 2015",
 			"July 2014",
 			"July 2012"
@@ -142,11 +180,11 @@ function setLanguage(language) {
 	image.src = (language === "fr")?"../img/france_flag.svg":"../img/uk_flag.svg";
 	document.getElementById("description").innerHTML = (language === "fr")?"Étudiant à l'ENSIMAG":"Student at ENSIMAG";
 	for (var classe in dictionary[language]) {
-		changeClassValues(classe, dictionary[language][classe]);
+		setClassValues(classe, dictionary[language][classe]);
 	}
 }
 
-function changeClassValues(classe, values) {
+function setClassValues(classe, values) {
 	var classes = document.getElementsByClassName(classe);
 	for (var i = 0; i < classes.length; i++) {
 		if (values[i] !== '') {
