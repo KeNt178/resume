96309b3091c0481bcf8f0cd472360accee22b787
Quentin Lapointe <q.lapointe@gmail.com>
2017-10-23 22:45:24 +0200

    ajout experience rgx

--- src/public/js/languages.js
+++ src/public/js/languages.js
@@ -11,6 +11,7 @@ var dictionary =
 			""
 		],
 		"name":[
+			"Vision par ordinateur pour l'agriculture de précision",
 			"POC sur <a href=\"http://neo4j.com/\"><i class=\"fa fa-link\"></i> Neo4j</a>",
 			"POC sur <a href=\"http://neo4j.com/\"><i class=\"fa fa-link\"></i> Neo4j</a>",
 			"Application iOS",
@@ -40,7 +41,8 @@ var dictionary =
 			"Passions"
 		],
 		"description":[
-			"E-marketing à l'aide d'une base de donnée graphes<ul><li>Liaisons des données CRM et de navigation Web de différents clients</li><li>Récupération d'une liste de contacts par calcul du chemin le moins cher en fonction d'une matrice de prix</li></ul>",
+			"Classification d'images hyperspectrales avec des techniques d'intelligence artificielle",
+			"E-marketing à l'aide d'une base de données graphes<ul><li>Liaisons des données CRM et de navigation Web de différents clients</li><li>Récupération d'une liste de contacts par calcul du chemin le moins cher en fonction d'une matrice de prix</li></ul>",
 			"Détection d'incohérences au sein d'une BD relationnelle en la transposant en BD orientée graphes<ul><li>Modélisation de la BD & ingestion des données</li><li>Détection d'incohérences & propositions de rectification</li><li>Statistiques sur les audiences Internet multi-support</li></ul>",
 			"Développement de <a href=\"https://itunes.apple.com/fr/app/togafmore/id581774033\"><i class=\"fa fa-link\"></i> TogafMore</a>",
 			"",
@@ -71,6 +73,7 @@ var dictionary =
 			"",
 			"",
 			"",
+			"",
 
 			"",
 			"IUT de Vélizy-Villacoublay",
@@ -78,6 +81,7 @@ var dictionary =
 			""
 		],
 		"date":[
+			"Mai – Août 2017",
 			"Juin – Juillet 2016",
 			"Avril – Juillet 2015",
 			"Juillet 2014",
@@ -101,6 +105,7 @@ var dictionary =
 			""
 		],
 		"name":[
+			"Computer vision for precision agriculture",
 			"POC on <a href=\"http://neo4j.com/\"><i class=\"fa fa-link\"></i> Neo4j</a>",
 			"POC on <a href=\"http://neo4j.com/\"><i class=\"fa fa-link\"></i> Neo4j</a>",
 			"iOS application",
@@ -130,6 +135,7 @@ var dictionary =
 			""
 		],
 		"description":[
+			"Classification of hyperspectral images with aritificial intelligence techniques",
 			"E-marketing with graph-oriented database<ul><li>Linking CRM and Web navigation data from several clients</li><li>Collecting a list of contacts by computing the least expensive path according to a price matrix</li></ul>",
 			"Inconsistencies detection in a relational database by transposing it in a graph oriented database<ul><li>Database modeling & data ingestion</li><li>Inconsistencies detection & rectification proposals</li><li>Statistics over multisupport Internet audiences</li></ul>",
 			"Development of <a href=\"https://itunes.apple.com/fr/app/togafmore/id581774033\"><i class=\"fa fa-link\"></i> TogafMore</a>",
@@ -161,6 +167,7 @@ var dictionary =
 			"",
 			"",
 			"",
+			"",
 
 			"",
 			"UIT of Vélizy-Villacoublay",
@@ -168,6 +175,7 @@ var dictionary =
 			""
 		],
 		"date":[
+			"May - August 2017",
 			"June - July 2016",
 			"April - July 2015",
 			"July 2014",
@@ -215,7 +223,7 @@ function setClassValues(classe, values) {
 	for (var i = 0; i < classes.length; i++) {
 		if (values[i] !== '') {
 			classes[i].innerHTML = values[i];
-			if (classe === 'date' & i > 3) {
+			if (classe === 'date' & i > 4) {
 				classes[i].innerHTML += '<span class="bullet">&#8226;</span>';
 			}
 		}
