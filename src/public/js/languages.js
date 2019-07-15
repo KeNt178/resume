76259944f7c55a5b1b4a047dd52017c7247ee26b
Quentin <q.lapointe@gmail.com>
2019-07-15 22:06:39 +0100

    yml data, removed all tabs, srb & vauban on map

--- src/public/js/languages.js
+++ src/public/js/languages.js
@@ -1,143 +1,56 @@
-var dictionary =
-{
-	"fr":{
-		"title":[
-			"<i class=\"fa fa-suitcase\"></i>Expériences professionnelles",
-			"<i class=\"fa fa-graduation-cap\"></i>Education",
-			"<i class=\"fa fa-suitcase\"></i>Projets personnels"
-		],
-		"name":[
-			"Vision par ordinateur pour l'agriculture de précision",
-			"POC sur <a href=\"http://neo4j.com/\"><i class=\"fa fa-link\"></i> Neo4j</a>",
-			"POC sur <a href=\"http://neo4j.com/\"><i class=\"fa fa-link\"></i> Neo4j</a>",
-			"Application iOS",
-
-			"Cursus d'ingénieur informatique",
-			"DUT Informatique",
-
-			"",
-			"",
-			"",
-			""
-		],
-		"description":[
-			"Classification d'images avec des techniques d'intelligence artificielle",
-			"E-marketing à l'aide d'une base de données graphes<ul><li>Liaisons des données CRM et de navigation Web de différents clients</li><li>Récupération d'une liste de contacts par calcul du chemin le moins cher en fonction d'une matrice de prix</li></ul>",
-			"Détection d'incohérences au sein d'une BD relationnelle en la transposant en BD orientée graphes<ul><li>Modélisation de la BD & ingestion des données</li><li>Détection d'incohérences & propositions de rectification</li><li>Statistiques sur les audiences Internet multi-support</li></ul>",
-			"Développement de <a href=\"https://itunes.apple.com/fr/app/togafmore/id581774033\"><i class=\"fa fa-link\"></i> TogafMore</a>",
-
-			"Un site de pari en cryptomonnaie",
-			"Un environnement Docker pour ouvrir en sécurité des images, vidéos, sons et plus",
-			"Extension Chrome qui permet le partage de lien en 1 clic via Facebook Messenger",
-			"Extension Chrome qui supprime toute publicité ou contenu sponsorisé sur Facebook et Google"
-		],
-		"place":[
-			"",
-			"",
-			"",
-			"",
-
-			"IUT de Vélizy-Villacoublay",
-			"Université de Technologie de Compiègne"
-		],
-		"date":[
-			"Mai – Août 2017",
-			"Juin – Juillet 2016",
-			"Avril – Juillet 2015",
-			"Juillet 2014",
-
-			"",
-			""
-		]
-	},
-	"en":{
-		"title":[
-			"<i class=\"fa fa-suitcase\"></i>Work experiences",
-			"<i class=\"fa fa-graduation-cap\"></i>Education",
-			"<i class=\"fa fa-flag\"></i>Personal projects"
-		],
-		"name":[
-			"Computer vision for precision agriculture",
-			"POC on <a href=\"http://neo4j.com/\"><i class=\"fa fa-link\"></i> Neo4j</a>",
-			"POC on <a href=\"http://neo4j.com/\"><i class=\"fa fa-link\"></i> Neo4j</a>",
-			"iOS application",
-
-			"Computer engineer curriculum",
-			"Two-year university degree in computer science",
-
-			"",
-			"",
-			"",
-			""
-		],
-		"description":[
-			"Classification of images with aritificial intelligence techniques",
-			"E-marketing with graph-oriented database<ul><li>Linking CRM and Web navigation data from several clients</li><li>Collecting a list of contacts by computing the least expensive path according to a price matrix</li></ul>",
-			"Inconsistencies detection in a relational database by transposing it in a graph oriented database<ul><li>Database modeling & data ingestion</li><li>Inconsistencies detection & rectification proposals</li><li>Statistics over multisupport Internet audiences</li></ul>",
-			"Development of <a href=\"https://itunes.apple.com/fr/app/togafmore/id581774033\"><i class=\"fa fa-link\"></i> TogafMore</a>",
-
-			"A bet website using cryptocurrencies",
-			"A virtual Docker environment to open securely images, videos, sounds and more",
-			"Chrome extension that lets you 1-click share your active tab through Facebook Messenger",
-			"Chrome extension that removes any ad or sponsored content on Facebook and Google"
-		],
-		"place":[
-			"",
-			"",
-			"",
-			"",
-
-			"UIT of Vélizy-Villacoublay",
-			"University of Technology of Compiègne"
-		],
-		"date":[
-			"May - August 2017",
-			"June - July 2016",
-			"April - July 2015",
-			"July 2014",
-
-			"",
-			""
-		]
-	}
+function loadData(locale) {
+  var file = "../js/languages.yml"
+  var rawFile = new XMLHttpRequest();
+  rawFile.onreadystatechange = function () {
+    if(rawFile.readyState === 4) {
+      if(rawFile.status === 200 || rawFile.status == 0) {
+        var allText = rawFile.responseText;
+        var yml = jsyaml.load(allText);
+        setLanguage(locale, yml[locale])
+      }
+    }
+  }
+	rawFile.open("GET", file);
+  rawFile.send(null);
 }
 
 function switchLanguage() {
-	if ($('input:checked').length === 1) {
-		$('input').prop('checked', true);
-		setLanguageWithAnimation("en");
-	} else {
-		setLanguageWithAnimation("fr");
-	}
+  if ($('input:checked').length === 1) {
+    $('input').prop('checked', true);
+    setLanguageWithAnimation("en");
+  } else {
+    setLanguageWithAnimation("fr");
+  }
 }
 
-function setLanguageWithAnimation(language) {
-	var set = false;
-	$('.name, .date, .place, .description, .title, #description').fadeOut(function() {
-		if (!set) {
-			setLanguage(language);
-			set = true;
-		}
-	});
-	$('.name, .date, .place, .description, .title, #description').fadeIn(1500);
+function setLanguageWithAnimation(locale) {
+  var set = false;
+  $('.name, .date, .place, .description, .title, #description').fadeOut(function() {
+    if (!set) {
+      loadData(locale);
+      set = true;
+
+    }
+  });
+  $('.name, .date, .place, .description, .title, #description').fadeIn(1500);
 }
 
-function setLanguage(language) {
-	$("#description").text((language == 'fr') ? "Intelligence artificielle & Blockchain" : "Artificial intelligence & Blockchain");
-	$("#switchflag").css('background-image', 'url("../img/' + language + '_flag.svg"');
-	for (var classe in dictionary[language]) {
-		setClassValues(classe, dictionary[language][classe]);
-	}
+async function setLanguage(locale, data) {
+  $("#description").text((locale == 'fr') ? "Intelligence artificielle & Blockchain" : "Artificial intelligence & Blockchain");
+  $("#switchflag").css('background-image', 'url("../img/' + locale + '_flag.svg"');
+  for (var classe in data) {
+    setClassValues(classe, data[classe]);
+  }
 }
 
 function setClassValues(classe, values) {
-	var classes = document.getElementsByClassName(classe);
-	for (var i = 0; i < classes.length; i++) {
-		if (values[i] !== '') {
-			classes[i].innerHTML = values[i];
-			if (classe === 'date' & i <= 3) {
-				classes[i].innerHTML += '<span class="bullet">&#8226;</span>';
-			}
-		}
-	}
+  var classes = document.getElementsByClassName(classe);
+  for (var i = 0; i < classes.length; i++) {
+    classes[i].innerHTML = values[i];
+
+    // bullets
+    if (classe === 'date' & i >= 2) {
+      classes[i].innerHTML += '<span class="bullet">&#8226;</span>';
+    }
+  }
 }
--- src/public/js/map.js
+++ src/public/js/map.js
@@ -1,86 +1,119 @@
 function initMap() {
-	var center = {lat: 46.5818414, lng: 2.5530366};
-	// var center = {lat: 46.6936371, lng: -1.5504454};
-	var education = {
-		url: '../img/education.svg',
-		scaledSize: new google.maps.Size(30, 30),
-		anchor: new google.maps.Point(15, 45)
-	}
-	var ensimag = {
-		position: {lat: 45.1933905, lng: 5.7682998},
-		icon: education
-	}
-	var velizy = {
-		position: {lat: 48.781856, lng: 2.218230},
-		icon: education
-	}
-	var utc =  {
-		position: {lat: 49.4155636, lng: 2.8184277},
-		icon: education
-	}
+  var center = {lat: 39.804742, lng: 52.5776676};
 
-	var mediametrie = {
-		position: {lat: 48.8970207, lng: 2.2873923},
-		icon: {
-			url: '../img/mediametrie.png',
-			scaledSize: new google.maps.Size(26, 26),
-			anchor: new google.maps.Point(13, 43)
-		}
-	}
-	var arismore = {
-		position: {lat: 48.8417505, lng: 2.2197262},
-		icon: {
-			url: '../img/arismore.png',
-			scaledSize: new google.maps.Size(34, 34),
-			anchor: new google.maps.Point(17, 47)
-		}
-	}
-	var cabestan = {
-		position: {lat: 48.8486972, lng: 2.2814019},
-		icon: {
-			url: '../img/cabestan.svg',
-			scaledSize: new google.maps.Size(34, 34),
-			anchor: new google.maps.Point(17, 47)
-		}
-	}
-	var rgx1 = {
-		position: {lat: 43.6270362, lng: 7.0386743},
-		icon: {
-			url: '../img/rgx.png',
-			scaledSize: new google.maps.Size(34, 34),
-			anchor: new google.maps.Point(17, 47)
-		}
-	}
+  var education = {
+    url: '../img/education.svg',
+    scaledSize: new google.maps.Size(30, 30),
+    anchor: new google.maps.Point(15, 45)
+  }
 
-	var parachute = {
-		position: {lat: 47.9020811, lng: 2.1688557},
-		icon: {
-			url: '../img/parachute.svg',
-			scaledSize: new google.maps.Size(66, 39),
-			anchor: new google.maps.Point(33, 47)
-		}
-	}
+  var ensimag = {
+    position: { lat: 45.1933905, lng: 5.7682998 },
+    icon: education
+  }
 
-	var places =  [ensimag, arismore, cabestan, rgx1, mediametrie]; // parachute
-	var colors = ['orange', 'black', 'blue', 'grey', 'green']; // 'grey'
+  var velizy = {
+    position: { lat: 48.781856, lng: 2.218230 },
+    icon: education
+  }
 
-	var mapDiv = document.getElementById("map");
-	var map = new google.maps.Map(mapDiv, {center: center, zoom: 5});
+  var utc =  {
+    position: { lat: 49.4155636, lng: 2.8184277 },
+    icon: education
+  }
 
-	var zindex = 0;
-	for (var i = 0; i < places.length; i++) {
-		places[i].map = map;
-		var marker = new google.maps.Marker({position: places[i].position, map: map, icon: {url: '../img/map_icon_' + colors[i] + '.svg', scaledSize: new google.maps.Size(50, 50)}, zIndex: zindex++});
-		places[i].zIndex = zindex++;
-		var marker2 = new google.maps.Marker(places[i]);
-		google.maps.event.addListener(marker, "click", function(evenement) {
-			map.panTo(evenement.latLng);
-			map.setZoom(19);
-		});
-		google.maps.event.addListener(marker2, "click", function(evenement) {
-			map.panTo(evenement.latLng);
-			map.setZoom(19);
-		});
-		zindex++;
-	}
+  var mediametrie = {
+    position: { lat: 48.8970207, lng: 2.2873923 },
+    icon: {
+      url: '../img/mediametrie.png',
+      scaledSize: new google.maps.Size(26, 26),
+      anchor: new google.maps.Point(13, 43)
+    }
+  }
+
+  var arismore = {
+    position: { lat: 48.8417505, lng: 2.2197262 },
+    icon: {
+      url: '../img/arismore.png',
+      scaledSize: new google.maps.Size(34, 34),
+      anchor: new google.maps.Point(17, 47)
+    }
+  }
+
+  var cabestan = {
+    position: {lat: 48.8486972, lng: 2.2814019},
+    icon: {
+      url: '../img/cabestan.svg',
+      scaledSize: new google.maps.Size(34, 34),
+      anchor: new google.maps.Point(17, 47)
+    }
+  }
+
+  var rgx1 = {
+    position: { lat: 43.6270362, lng: 7.0386743 },
+    icon: {
+      url: '../img/rgx.png',
+      scaledSize: new google.maps.Size(34, 34),
+      anchor: new google.maps.Point(17, 47)
+    }
+  }
+
+  var parachute = {
+    position: { lat: 47.9020811, lng: 2.1688557 },
+    icon: {
+      url: '../img/parachute.svg',
+      scaledSize: new google.maps.Size(66, 39),
+      anchor: new google.maps.Point(33, 47)
+    }
+  }
+
+  var srb1 = {
+    position: { lat: 48.875357, lng: 2.3493883 },
+    icon: {
+      url: '../img/srb.jpg',
+      scaledSize: new google.maps.Size(28, 28),
+      anchor: new google.maps.Point(14, 45)
+    }
+  }
+
+  var srb2 = {
+    position: { lat: 11.5747761, lng: 104.9046362 },
+    icon: {
+      url: '../img/srb.jpg',
+      scaledSize: new google.maps.Size(28, 28),
+      anchor: new google.maps.Point(14, 45)
+    }
+  }
+
+  var vauban = {
+    position: { lat: 51.5273313, lng: -0.0885324 },
+    icon: {
+      url: '../img/vauban.png',
+      scaledSize: new google.maps.Size(28, 28),
+      anchor: new google.maps.Point(14, 45)
+    }
+  }
+
+  var places =  [srb1, srb2, ensimag, arismore, cabestan, rgx1, mediametrie, vauban]; // parachute
+  var colors = ['white', 'white', 'orange', 'black', 'blue', 'grey', 'green', 'white']; // 'grey'
+
+  var mapDiv = document.getElementById("map");
+  var map = new google.maps.Map(mapDiv, { center: center, zoom: 3 });
+
+  var zindex = 0;
+  for (var i = 0; i < places.length; i++) {
+    places[i].map = map;
+    var marker = new google.maps.Marker({ position: places[i].position, map: map, icon: { url: '../img/map_icon_' + colors[i] + '.svg', scaledSize: new google.maps.Size(50, 50) }, zIndex: zindex++ });
+    places[i].zIndex = zindex++;
+    var marker2 = new google.maps.Marker(places[i]);
+    google.maps.event.addListener(marker, "click", function(evenement) {
+      map.panTo(evenement.latLng);
+      map.setZoom(19);
+    });
+    google.maps.event.addListener(marker2, "click", function(evenement) {
+      map.panTo(evenement.latLng);
+      map.setZoom(19);
+    });
+    zindex++;
+  }
 }
