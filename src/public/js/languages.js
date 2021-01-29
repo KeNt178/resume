82d96df9676e25ac7b00f4e572e526bf81b58185
lapwat <q.lapointe@gmail.com>
2021-01-29 17:46:09 +0100

    skills intro, update email

--- src/public/js/languages.js
+++ src/public/js/languages.js
@@ -25,19 +25,21 @@ function switchLanguage() {
 
 function setLanguageWithAnimation(locale) {
   var set = false;
-  $('.name, .date, .place, .description, .title, #description').fadeOut(function() {
+  $('.name, .date, .place, .description, .title, #skills, #wannalearn').fadeOut(function() {
     if (!set) {
       loadData(locale);
       set = true;
 
     }
   });
-  $('.name, .date, .place, .description, .title, #description').fadeIn(1500);
+  $('.name, .date, .place, .description, .title, #skills, #wannalearn').fadeIn(1500);
 }
 
 async function setLanguage(locale, data) {
   $("#description").text((locale == 'fr') ? "Data & DevSecOps" : "Data & DevSecOps");
   $("#switchflag").css('background-image', 'url("../img/' + locale + '_flag.svg"');
+  $("#skills").text((locale == 'fr') ? "Compétences" : "Skills");
+  $("#wannalearn").text((locale == 'fr') ? "Souhaits de formation" : "Formation wishes");
   for (var classe in data) {
     setClassValues(classe, data[classe]);
   }
